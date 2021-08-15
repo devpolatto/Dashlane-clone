8cbd71aebf9b437fadec0dcf4308b3f4278fa58f
Angelo Polatto <polatto.xcode.job@gmail.com>
2021-08-15 17:56:57 -0300

    Finished Header Lauyout

--- src/components/Button/styles.tsx
+++ src/components/Button/styles.tsx
@@ -1,16 +1,40 @@
 import styled from 'styled-components';
+import { ReactNode } from 'react';
 
-export const ButtonStyled = styled.button`
+interface ButtonProps  {
+    typeColor?: string;
+    children?: ReactNode
+}
+
+export const ButtonStyled = styled.button<ButtonProps>`
     display: flex;
     align-items: center;
+    height: 40px;
+    width: auto;
 
     padding: 10px 16px;
+    border: none;
     border-radius: 4px;
-    border-color: var(--color-background-button);
 
     display: flex;
     gap: 8px;
     
-    background-color: var(--color-background-button);
-    color: var(--color-text-theme)
+    background-color: ${props => props.typeColor === 'primary'
+        ? 'var(--color-button-primary)'
+        : 'var(--color-button-secundary)'
+    };
+    border: 1px solid var(--color-button-primary);
+    color: ${ props => props.typeColor === 'primary'
+        ? 'var(--color-text-button-primary)'
+        : 'var(--color-text-button-secundary)'
+    };
+
+    transition: 0.2s;
+
+    &:hover{
+        background-color: ${ props => props.typeColor === 'primary'
+            ? '#093E49'
+            : '#D8E6E9'
+        };
+    }
 `;
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,15 +4,25 @@ import Button from '../Button';
 
 import { 
     Container,
-    PlusIcon
+    PlusIcon,
+    ShareIcon,
+    ChangeAllIcon,
+    BellIcon
 } from './styles';
 
 const Header: React.FC = () => {
   return(
       <Container>
-        <Button text="Add new">
-            <PlusIcon/>
-        </Button>
+          <Button typeColor="primary" text="Add new">
+              <PlusIcon/>
+          </Button>
+          <Button text="Share">
+              <ShareIcon color="#0e353d"/>
+          </Button>
+          <Button text="Change Passwords">
+              <ChangeAllIcon color="#0e353d"/>
+          </Button>
+          <BellIcon color="#0e353d"/>
       </Container>
   );
 }
